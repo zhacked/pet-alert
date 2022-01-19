d1c8e70b3ebbfb9b7a34bf806e1cbe2ec6ed6830
enan <fsiapco@gmail.com>
2022-01-19 09:50:00 +0800

    fixin
    teest

--- routes/web.php
+++ routes/web.php
@@ -24,13 +24,13 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
-Auth::routes();
 
+Route::middleware('preventBackHistory')->group(function () {
+Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('{path}', [App\Http\Controllers\HomeController::class, 'index'])->where('path', '([A-z]+)?');
 
 Route::post('/forgot-passwords', [App\Http\Controllers\API\ResetPasswordController::class, 'index'])->name('forgotMe');
 
-
-// Route::get('sms', [App\Http\Controllers\SmsController::class, ''])
+});
