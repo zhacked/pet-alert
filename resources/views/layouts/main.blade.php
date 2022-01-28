acd83e80af42b0390570d2f910e68e13ed6b7c87
enan <fsiapco@gmail.com>
2022-01-28 11:02:38 +0800

    fixin

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,7 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
 
-	<title>{{ config('app.name', 'Laravel') }}</title>
+	<title>{{ config('app.name', 'Pet Allert') }}</title>
 	<link rel="shortcut icon" href="{{ asset('image/favicon.ico') }}">
 	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 	<link href="{{ asset('css/custom.css') }}" rel="stylesheet">
